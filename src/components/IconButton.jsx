259ddd87f1c7821b7ac3846a5179503f5584de75
buletomato25 <pinriko2515@gmail.com>
2025-04-09 20:19:15 +0900

    suwabe_0409

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,13 +1,13 @@
-import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
-import Stack from '@mui/material/Stack';
-import DehazeIcon from '@mui/icons-material/Dehaze';
+import * as React from "react";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+import DehazeIcon from "@mui/icons-material/Dehaze";
 
 export default function IconButtons({ onClick }) {
   return (
     <Stack direction="row" spacing={1}>
-      <IconButton aria-label="Dehaze" onClick={onClick} sx={{ color: 'white' }}>
-        <DehazeIcon />
+      <IconButton aria-label="Dehaze" onClick={onClick} sx={{ color: "white" }}>
+        <DehazeIcon sx={{ fontSize: 40 }} />
       </IconButton>
     </Stack>
   );
